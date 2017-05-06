dd979374f9db0cfb0cdc758343c3bbe1bb8ef5bb
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-05-06 17:13:29 -0400

    Finished objectifying board

--- lib/settlers_of_catan/game_board.rb
+++ lib/settlers_of_catan/game_board.rb
@@ -4,14 +4,14 @@ class SettlersOfCatan::GameBoard
     def initialize
         @facet = []
         54.times do |i|
-             @facet << SettlersOfCatan::Facet.new(i+1)
+            @facet << SettlersOfCatan::Facet.new(i+1)
         end
         @edge = []
         90.times do |i|
             dirs = ["horiz","fwd","back"]
-        @edge << SettlersOfCatan::Edge.new(i,dirs[i%3])
+            @edge << SettlersOfCatan::Edge.new(i,dirs[i%3])
         end
-        
+
         @tile = []
         @tile_numbers = [5,2,6,3,8,10,9,12,11,4,8,10,9,4,5,6,3,11]
         @tile_resources = ["ore", "ore", "ore", "brick", "brick", "brick",  "wheat", "wheat", "wheat", "wheat", "sheep", "sheep", "sheep", "sheep", "wood", "wood", "wood", "wood", "dessert"]
@@ -28,7 +28,7 @@ class SettlersOfCatan::GameBoard
 
         @colors = {:red => "\e[31m",:green => "\e[32m",:yellow => "\e[33m",:blue => "\e[34m",:magenta => "\e[35m",:cyan => "\e[36m",:default => "\e[39m", :gray => "\e[90m", :light_green => "\e[92m"}
 
-        
+
     end
     def current_board
         puts "\e[H\e[2J"
@@ -50,7 +50,20 @@ class SettlersOfCatan::GameBoard
         puts " "*5 + @edge[25].show + @facet[24].show + " "*3 + @tile[8].show+ " "*4 + @edge[26].show + @facet[25].show + @edge[36].show+ @edge[28].show+ @facet[26].show + " "*3 + @tile[9].show+ " "*4+ @edge[29].show + @facet[27].show + @edge[39].show + @edge[31].show + @facet[28].show + " "*3 + @tile[10].show + " "*4 + @edge[32].show + @facet[29].show
         puts " "*5 + @edge[53].show + " "*11 + @edge[37].show + " "*7 + @edge[38].show + " "*11 + @edge[40].show+ " "*7 + @edge[41].show + " "*11 + @edge[52].show
         puts " "*6 + @edge[53].show + " "*9 + @edge[37].show + " "*9 + @edge[38].show + " "*9 + @edge[40].show+ " "*9 + @edge[41].show + " "*9 + @edge[52].show
-        puts " "*7 + @edge[53].show + @facet[18].show + @edge[24].show + @edge[37].show + @facet[19].show + " "*3 + @tile[11].show + " "*4 + @edge[38].show + @facet[20].show + @edge[27].show + @edge[40].show + @facet[21].show + " "*3 + @tile[12].show + " "*4 + @edge[41].show + @facet[22].show + @edge[30].show + @edge[52].show + @facet[23].show
+        puts " "*7 + @edge[53].show + @facet[30].show + @edge[42].show + @edge[37].show + @facet[31].show + " "*3 + @tile[11].show + " "*4 + @edge[38].show + @facet[32].show + @edge[45].show + @edge[40].show + @facet[33].show + " "*3 + @tile[12].show + " "*4 + @edge[41].show + @facet[34].show + @edge[48].show + @edge[52].show + @facet[35].show
+        puts " "*7 + @edge[43].show + " "*7 + @edge[44].show+ " "*11 + @edge[46].show+ " "*7 + @edge[47].show + " "*11 + @edge[49].show + " "*7 + @edge[50].show
+        puts " "*6 + @edge[43].show + " "*9 + @edge[44].show+ " "*9 + @edge[46].show+ " "*9 + @edge[47].show + " "*9 + @edge[49].show + " "*9 + @edge[50].show
+        puts " "*5 + @edge[43].show + @facet[36].show + " "*3 + @tile[13].show+ " "*4 + @edge[44].show + @facet[37].show + @edge[54].show+ @edge[46].show+ @facet[38].show + " "*3 + @tile[14].show+ " "*4+ @edge[47].show + @facet[39].show + @edge[57].show + @edge[49].show + @facet[40].show + " "*3 + @tile[15].show + " "*4 + @edge[50].show + @facet[41].show
+        puts " "*5 + @edge[62].show + " "*11 + @edge[55].show + " "*7 + @edge[56].show + " "*11 + @edge[58].show+ " "*7 + @edge[59].show + " "*11 + @edge[61].show
+        puts " "*6 + @edge[62].show + " "*9 + @edge[55].show + " "*9 + @edge[56].show + " "*9 + @edge[58].show+ " "*9 + @edge[59].show + " "*9 + @edge[61].show
+        puts " "*7 + @edge[62].show + @facet[42].show + @edge[42].show + @edge[55].show + @facet[43].show + " "*3 + @tile[16].show + " "*4 + @edge[56].show + @facet[44].show + @edge[63].show + @edge[58].show + @facet[45].show + " "*3 + @tile[17].show + " "*4 + @edge[59].show + @facet[46].show + @edge[66].show + @edge[61].show + @facet[47].show
+        puts " "*15 + @edge[68].show + " "*11 + @edge[64].show + " "*7 + @edge[65].show + " "*11 + @edge[67].show
+        puts " "*16 + @edge[68].show + " "*9 + @edge[64].show + " "*9 + @edge[65].show + " "*9 + @edge[67].show    
+        puts " "*17 + @edge[68].show + @facet[48].show + @edge[69].show + @edge[64].show + @facet[49].show + " "*3 + @tile[18].show + " "*4 + @edge[65].show + @facet[50].show + @edge[72].show + @edge[67].show + @facet[51].show
+        puts " "*25 + @edge[71].show + " "*11 + @edge[73].show
+        puts " "*26 + @edge[71].show + " "*9 + @edge[73].show
+        puts " "*27 + @edge[71].show + @facet[52].show + @edge[75].show + @edge[73].show + @facet[53].show
+        
         puts "\n \n \n"
         puts "Legend: [roygbw] = Settlement, [ROYGBW] = City"
     end
