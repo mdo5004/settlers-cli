97221b2d6c6708a1468c790abfde136c0aa40389
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-17 16:14:14 -0400

    Added background colors

--- lib/settlers_of_catan/concerns/color.rb
+++ lib/settlers_of_catan/concerns/color.rb
@@ -1,10 +1,10 @@
 class Color
     attr_reader :colors, :resource_colors
     @@colors = {:red => "\e[31m",:green => "\e[32m",:yellow => "\e[33m",:blue => "\e[34m",:magenta => "\e[35m",:cyan => "\e[36m",:default => "\e[39m", :gray => "\e[37m", :light_green => "\e[92m", :white => "\e[97m", :black => "\e[30m"}
-    @@background_colors = {:red => "\e[41m",:green => "\e[42m",:yellow => "\e[43m",:blue => "\e[44m",:magenta => "\e[45m",:cyan => "\e[46m",:default => "\e[49m", :gray => "\e[47m", :light_green => "\e[102m", :white => "\e[107m", :dark_gray => "\e[100m", :black => "\e[40m"}
+    @@background_colors = {:red => "\e[41m",:green => "\e[42m",:yellow => "\e[43m",:blue => "\e[44m",:magenta => "\e[45m",:cyan => "\e[46m",:default => "\e[49m", :gray => "\e[100m", :light_green => "\e[102m", :white => "\e[47m", :dark_gray => "\e[100m", :black => "\e[40m", :orange => "\e[103m"}
     @@resource_colors = {:ore => :gray, :brick => :red, :wheat => :yellow, :sheep => :light_green, :wood => :green, :dessert => :default}
     
-    @@text_color_for_background = {:red => :white, :green => :white, :yellow => :white, :blue => :white, :magenta => :white, :cyan => :white, :default => :default, :gray => :black, :light_green => :white, :white => :white, :dark_gray => :white, :black => :white}
+    @@text_color_for_background = {:red => :white, :green => :white, :yellow => :white, :blue => :white, :magenta => :white, :cyan => :white, :default => :default, :gray => :white, :light_green => :white, :white => :black, :dark_gray => :white, :black => :white, :orange => :black}
     
     def self.colorize(str,display_color)
         if display_color
@@ -28,9 +28,11 @@ class Color
         if !str.is_a? String 
             str = str.to_s
         end
-        bkg = color ? @@background_colors[color.to_sym] : @@background_colors[:default]
-        txt =  color ? @@colors[@@text_color_for_background[color.to_sym]] : @@colors[:default]
-        return "#{bkg}#{txt}"
+        bkg = @@background_colors[color.to_sym]
+        txt = @@colors[@@text_color_for_background[color.to_sym]]
+        color = "#{bkg}#{txt}"
+        display = self.colorize(str,color)
+        return display
     end
     def self.colorize_by_resource(str,resource)
         if !str.is_a? String 
