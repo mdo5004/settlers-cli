22ace7508ba005a072a9ede7bc61a2f5d61696d9
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-05-17 16:12:09 -0400

    Began adding tests

--- spec/settlers_of_catan_spec.rb
+++ spec/settlers_of_catan_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe SettlersOfCatan do
     expect(SettlersOfCatan::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "can setup a new game" do
+      game = SettlersOfCatan::Game.new
+      board = game.board
+      expect(board).to be_a(SettlersOfCatan::GameBoard)
   end
+    
+    it "plays a whole game" do
+        game = SettlersOfCatan::Game.new
+        result = game.start
+        expect(result).to be_truthy
+    end
+    
 end
